f48bcbea530784614566eeffb56962e3e6de6704
Noobman15025 <shahariarshawon.dev@gmail.com>
2026-01-28 16:08:52 +0600

    project completed

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,6 +1,19 @@
-export const USER_API_END_POINT = "http://localhost:5001/api/user";
-export const JOB_API_END_POINT = "http://localhost:5001/api/job";
-export const APPLICATION_API_END_POINT = "http://localhost:5001/api/application";
-export const COMPANY_API_END_POINT = "http://localhost:5001/api/company";
-export const BLOG_API_END_POINT = "http://localhost:5001/api/blogs";
-export const NEWSLETTER_API_END_POINT = "http://localhost:5001/api/newsletter";
+const RAW_BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
+
+const BASE_URL = `${RAW_BACKEND_URL}/api`;
+
+if (!BASE_URL) {
+  console.error(
+    "VITE_BACKEND_URL is NOT defined. Check Vercel Environment Variables."
+  );
+}
+
+// API Endpoints
+export const USER_API_END_POINT = `${BASE_URL}/user`;
+export const JOB_API_END_POINT = `${BASE_URL}/job`;
+export const APPLICATION_API_END_POINT = `${BASE_URL}/application`;
+export const COMPANY_API_END_POINT = `${BASE_URL}/company`;
+export const BLOG_API_END_POINT = `${BASE_URL}/blogs`;
+export const NEWSLETTER_API_END_POINT = `${BASE_URL}/newsletter`;
+
+console.log("BASE_URLLL:", BASE_URL);
